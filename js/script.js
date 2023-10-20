e9589e741517c8e4ec2464f420ae5f1c98e78f9b
Suraj Yadav <itssurajyadav7@gmail.com>
2023-10-20 12:04:13 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 /*==================== typed js ====================*/
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer' ],
+    strings: ['Web Developer' ],
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 1000,
