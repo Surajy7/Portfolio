92b109847c23d09181126899b64bb4880fc33b04
Suraj Yadav <itssurajyadav7@gmail.com>
2023-11-29 09:20:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ menuIcon.onclick = () => {
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () => {
+window.onscroll = () => { 
     sections.forEach(sec => {
         let top = window.scrollY;
         let Offset = sec.offsetTop - 150;
